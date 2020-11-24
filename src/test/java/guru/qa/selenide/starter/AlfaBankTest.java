780994cef4a6fa135468b2374efef2daa6d87408
stanislavd <stanislav.dmitruk@neugelb.com>
2020-11-24 04:14:50 +0100

    Updates to AlfaBankTest

--- src/test/java/guru/qa/selenide/starter/AlfaBankTest.java
+++ src/test/java/guru/qa/selenide/starter/AlfaBankTest.java
@@ -1,12 +1,18 @@
 package guru.qa.selenide.starter;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 public class AlfaBankTest {
 
     @Test
     void testAlfa(){
-        Selenide.open("")
+        Selenide.open("https://alfabank.ru/");
+        Selenide.$(Selectors.byText("Вклады")).click();
+        Selenide.$("body").shouldHave(Condition.text("Накопительные продукты"));
+
     }
 }
 
