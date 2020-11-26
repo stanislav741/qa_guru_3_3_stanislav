6d31d38f1a0d3edb12b535d3b2db725d5ab8513b
stanislavd <stanislav.dmitruk@neugelb.com>
2020-11-26 03:45:21 +0100

    Added steps for locating Deposits page and verifying displayed text

--- src/test/java/guru/qa/selenide/starter/AlfaBankTest.java
+++ src/test/java/guru/qa/selenide/starter/AlfaBankTest.java
@@ -5,14 +5,23 @@ import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 public class AlfaBankTest {
 
     @Test
     void testAlfa(){
-        Selenide.open("https://alfabank.ru/");
-        Selenide.$(Selectors.byText("Вклады")).click();
-        Selenide.$("body").shouldHave(Condition.text("Накопительные продукты"));
+        String varMenuButton = "Депозиты";
 
+        open("https://alfabank.ru/");
+        $(byText("Вклады")).click();
+        $("body").shouldHave(text("Накопительные продукты"));
+        $x("//div[@id='filter']//a[@data-widget-uid='0dc9994fe9']").click();
+//       $$(byText("Депозиты")).find(visible).click();  best option, not working
+//       $(byText("Депозиты"),1).click();   not working
+        $("body").shouldHave(text("Вклад на ваших условиях"));
     }
 }
 
